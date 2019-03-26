1e80da933ef9792065a67c78e0ce113c26b83113
apilink <apilink@yonyou.com>
2019-03-26 13:22:12 +0800

    更新 spring boot 的版本

--- src/main/java/com/yonyou/iuap/isv/demo/ServletInitializer.java
+++ src/main/java/com/yonyou/iuap/isv/demo/ServletInitializer.java
@@ -1,7 +1,7 @@
 package com.yonyou.iuap.isv.demo;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
